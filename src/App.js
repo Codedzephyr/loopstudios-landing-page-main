f23c46afc9a067e3fbc6c5208873e82f9e6c1eb7
unknown <salihu45@gmail.com>
2021-12-30 19:55:35 +0100

    grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
 import MiddleSection from "./Components/MiddleSection";
+import ThirdSection from "./Components/ThirdSection";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MiddleSection />
+      <ThirdSection />
     </div>
   );
 }
---
+++ src/Components/ThirdSection.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./main.css";
+
+const ThirdSection = () => {
+  return (
+    <section id="thirdSection">
+      <div className="topContainer">
+        <p className="leftContent">our creations</p>
+        <button className="buttonSeeDesktop">see all</button>
+      </div>
+      <div className="gridImageContainer">
+        <div className="one">
+          <p className="gridImageText">
+            deep <br /> earth
+          </p>
+        </div>
+        <div className="two">
+          <p className="gridImageText">
+            night <br /> arcade
+          </p>
+        </div>
+        <div className="three">
+          <p className="gridImageText">
+            soccer <br /> team vr
+          </p>
+        </div>
+        <div className="four">
+          <p className="gridImageText">
+            the <br /> grid
+          </p>
+        </div>
+        <div className="five">
+          <p className="gridImageText">
+            from up <br /> above vr
+          </p>
+        </div>
+        <div className="six">
+          <p className="gridImageText6">
+            pocket <br /> borealis
+          </p>
+        </div>
+        <div className="seven">
+          <p className="gridImageText7">
+            the <br /> curiosity
+          </p>
+        </div>
+        <div className="eight">
+          <p className="gridImageText">
+            make it <br /> fisheye
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ThirdSection;
